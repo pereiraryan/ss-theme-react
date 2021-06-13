7da2e2bb5b21db349335b9d45bba6cd7a96613cd
ryan.p <ryan.p@endurance.com>
2021-06-13 19:44:01 +0530

    Fixing netlify issue for a11y

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,45 @@
 import React from "react"
+import {
+    BrowserRouter as Router,
+    Link
+  } from "react-router-dom";
 
 function Header() {
   return (
+    <Router>
         <header className="Brand-header">
             <div className="primary-nav">
                 <div className="primary-wrapper">
                     <ul> 
-                        <li><a href="#" ><figure><img src="./assets/login.png" alt="Login" title="Login/Signup" className="Login" /></figure> Login/Signup</a></li>
-                        <li><a href="#" > Language: English<figure><img src="./assets/carrot.png" alt="Language" title="Language" className="carrot" /></figure></a></li>
-                        <li><a href="#" > Currency: BYR<figure><img src="./assets/carrot.png" alt="Language" title="Language" className="carrot"/></figure></a></li>
-                        <li><a href="#"><figure><img src="./assets/cart.png" alt="Cart" title="cart" className="cart"/></figure><span className="Cart cart-num" id="dashboard-cartItemCount">0</span></a></li>
+                        <li><Link to="/"><figure><img src="./assets/login.png" alt="Login" title="Login/Signup" className="Login" /></figure> Login/Signup</Link></li>
+                        <li><Link to="/"> Language: English<figure><img src="./assets/carrot.png" alt="Language" title="Language" className="carrot" /></figure></Link></li>
+                        <li><Link to="/"> Currency: BYR<figure><img src="./assets/carrot.png" alt="Language" title="Language" className="carrot"/></figure></Link></li>
+                        <li><Link to="/"><figure><img src="./assets/cart.png" alt="Cart" title="cart" className="cart"/></figure><span className="Cart cart-num" id="dashboard-cartItemCount">0</span></Link></li>
                     </ul>
                 </div>
             </div>
             <div className="container">       
                 <div className="logo-wrapper">
                     <figure>
-                        <a href="/"><img src="./assets/logo.png" alt="logo" title="Your Brand"/></a>
+                        <Link to="/"><img src="./assets/logo.png" alt="logo" title="Your Brand"/></Link>
                     </figure>
                 </div>
             </div>
             <nav>
-                <a className="burger-nav"></a>
+                <Link to="/" className="burger-nav"></Link>
                 <div className="nav-wrapper">
-                    <a href="/" title="home">home</a>
-                    <a href="">Domains</a>
-                    <a href="">Websites <span className="new-tag inline-block">New</span></a>
-                    <a href="" className="selected">Hosting</a>
-                    <a href="">Cloud</a>
-                    <a href="">Email and Productivity <span className="new-tag inline-block">New</span></a>
-                    <a href="">Security</a>
-                    <a href="">Combo Offers</a>
+                    <Link to="/" title="home">home</Link>
+                    <Link to="/">Domains</Link>
+                    <Link to="/">Websites <span className="new-tag inline-block">New</span></Link>
+                    <Link to="/" className="selected">Hosting</Link>
+                    <Link to="/">Cloud</Link>
+                    <Link to="/">Email and Productivity <span className="new-tag inline-block">New</span></Link>
+                    <Link to="/">Security</Link>
+                    <Link to="/">Combo Offers</Link>
                 </div>
             </nav>
         </header>
+    </Router>
   );
 }
 
--- src/components/Plans.js
+++ src/components/Plans.js
@@ -1,72 +1,78 @@
 import React from "react";
+import {
+    BrowserRouter as Router,
+    Link
+  } from "react-router-dom";
 
 function Plans() {
   return (
-    <section id="Plans-section" className="Plans">
-        <h2 className="Plans-heading">Get Started with Office 365</h2>
-        <div className="Slider-wrapper">
-            <input type="range" min="0" max="100" value="100" className="Slider-range Slider-dot" disabled /><input type="range" min="0" max="100" value="75" className="Slider-range Slider-disabled" disabled />
-            <div className="Slider-title"><span className="active">Select a Plan</span><span>Add Number of Licence</span></div>
-        </div>
-        <div className="Plans-wrapper">
-            <div className="Plan Plan1">
-                <h5 className="Plan-name">Email Essentials</h5>
-                <div className="Plan-price"><span>$</span>1.99.00<sub>/licence/mo</sub></div>
-                <div className="Plan-FeaturesList">
-                    <div className="FeaturesList">Outlook WebAccess<span>15GB Mailbox</span></div>
-                    <div className="Features-icon"><figure><img src="./assets/o365_outlook.png" alt="BU" title="Outlook"/></figure></div>
-                    <a href="#" className="Plan-cta">Select Plan</a>
+    <Router>
+            <section id="Plans-section" className="Plans">
+                <h2 className="Plans-heading">Get Started with Office 365</h2>
+                <div className="Slider-wrapper">
+                    <input type="range" min="0" max="100" value="100" className="Slider-range Slider-dot" disabled /><input type="range" min="0" max="100" value="75" className="Slider-range Slider-disabled" disabled />
+                    <div className="Slider-title"><span className="active">Select a Plan</span><span>Add Number of Licence</span></div>
                 </div>
-                
+                <div className="Plans-wrapper">
+                    <div className="Plan Plan1">
+                        <h5 className="Plan-name">Email Essentials</h5>
+                        <div className="Plan-price"><span>$</span>1.99.00<sub>/licence/mo</sub></div>
+                        <div className="Plan-FeaturesList">
+                            <div className="FeaturesList">Outlook WebAccess<span>15GB Mailbox</span></div>
+                            <div className="Features-icon"><figure><img src="./assets/o365_outlook.png" alt="BU" title="Outlook"/></figure></div>
+                            <Link to="/" className="Plan-cta">Select Plan</Link>
+                        </div>
+                        
 
-            </div>
-            <div className="Plan Plan2">
-                <h5 className="Plan-name">Business Essentials</h5>
-                <div className="Plan-price"><span>$</span>5.99<sub>/licence/mo</sub></div>
-                <div className="Plan-FeaturesList">
-                    <div className="FeaturesList">Outlook WebAccess
-                        <span>50 GB Mailbox </span>
                     </div>
-                    <div className="Features-icon"><figure><img src="./assets/o365_outlook.png" alt="Outlook" title="Outlook"/></figure></div>
-                    <div className="FeaturesList">Cloud Storage
-                        <span>1TB</span>
+                    <div className="Plan Plan2">
+                        <h5 className="Plan-name">Business Essentials</h5>
+                        <div className="Plan-price"><span>$</span>5.99<sub>/licence/mo</sub></div>
+                        <div className="Plan-FeaturesList">
+                            <div className="FeaturesList">Outlook WebAccess
+                                <span>50 GB Mailbox </span>
+                            </div>
+                            <div className="Features-icon"><figure><img src="./assets/o365_outlook.png" alt="Outlook" title="Outlook"/></figure></div>
+                            <div className="FeaturesList">Cloud Storage
+                                <span>1TB</span>
+                            </div>
+                            <div className="FeaturesList">Collaborate tools</div>
+                            <div className="Features-icon"><figure><img src="./assets/o365_teams.png" alt="team" title="Teams"/></figure><figure><img src="./assets/o365_sharepoint.png" alt="o365_sharepoint" title="sharepoint"/></figure><figure><img src="./assets/o365_yammer.png" alt="yammer" title="yammer"/></figure></div>
+                            <div className="FeaturesList">Microsoft Office online</div>
+                            <div className="Features-icon"><figure><img src="./assets/o365_word.png" alt="word" title="Word"/></figure><figure><img src="./assets/o365_excel.png" alt="excel" title="Excel"/></figure><figure><img src="./assets/o365_powerpoint.png" alt="o365_powerpoint" title="PowerPoint"/></figure></div>
+                            <Link to="/" className="Plan-cta">Select Plan</Link>
+                        </div>
+                        
+
                     </div>
-                    <div className="FeaturesList">Collaborate tools</div>
-                    <div className="Features-icon"><figure><img src="./assets/o365_teams.png" alt="team" title="Teams"/></figure><figure><img src="./assets/o365_sharepoint.png" alt="o365_sharepoint" title="sharepoint"/></figure><figure><img src="./assets/o365_yammer.png" alt="yammer" title="yammer"/></figure></div>
-                    <div className="FeaturesList">Microsoft Office online</div>
-                    <div className="Features-icon"><figure><img src="./assets/o365_word.png" alt="word" title="Word"/></figure><figure><img src="./assets/o365_excel.png" alt="excel" title="Excel"/></figure><figure><img src="./assets/o365_powerpoint.png" alt="o365_powerpoint" title="PowerPoint"/></figure></div>
-                    <a href="#" className="Plan-cta">Select Plan</a>
+                    <div className="Plan Plan3">
+                        <h5 className="Plan-name">Business Premium</h5>
+                        <div className="Plan-price"><span>$</span>9.99<sub>/licence/mo</sub></div>
+                        <div className="Plan-FeaturesList">
+                            <div className="FeaturesList">Outlook WebAccess
+                                <span>50 GB Mailbox </span>
+                            </div>
+                            <div className="Features-icon"><figure><img src="./assets/o365_outlook.png" alt="Outlook" title="Outlook"/></figure></div>
+                            <div className="FeaturesList">Cloud Storage
+                                <span>1TB</span>
+                            </div>
+                            <div className="FeaturesList">Collaborate tools</div>
+                            <div className="Features-icon"><figure><img src="./assets/o365_teams.png" alt="team" title="Teams"/></figure><figure><img src="/assets/o365_sharepoint.png" alt="o365_sharepoint" title="sharepoint"/></figure><figure><img src="./assets/o365_yammer.png" alt="yammer" title="yammer"/></figure></div>
+                            <div className="FeaturesList">Microsoft Office online</div>
+                            <div className="Features-icon"><figure><img src="./assets/o365_word.png" alt="word" title="Word"/></figure><figure><img src="./assets/o365_excel.png" alt="excel" title="Excel"/></figure><figure><img src="./assets/o365_powerpoint.png" alt="o365_powerpoint" title="PowerPoint"/></figure></div>
+                            <div className="FeaturesList"> Microsoft Office Desktop</div>
+                            <div className="Features-icon"><figure><img src="./assets/o365_word.png" alt="word" title="Word"/></figure><figure><img src="./assets/o365_excel.png" alt="excel" title="Excel"/></figure><figure><img src="./assets/o365_powerpoint.png" alt="o365_powerpoint" title="PowerPoint"/></figure><figure><img src="./assets/o365_outlook.png" alt="Outlook" title="Outlook"/></figure><figure><img src="./assets/o365_access.png" alt="Access" title="Access"/></figure><figure><img src="./assets/o365_one_note.png" alt="note" title="Notes"/></figure><figure><img src="./assets/o365_publisher.png" alt="Publisher" title="Publisher"/></figure></div>
+                            <div className="FeaturesList">Business Apps</div>
+                            <div className="Features-icon Features-Bizicon"><figure><img src="./assets/o365_outlook_customer.png" alt="Outlook" title="Outlook"/></figure><figure><img src="./assets/o365_bookings.png" alt="o365_bookings" title="o365_bookings"/></figure><figure><img src="./assets/o365_invoicing.png" alt="o365_invoicing" title="Invoice"/></figure><figure><img src="./assets/o365_mile-iq.png" alt="o365_mile-iq" title="Mile"/></figure></div>
+                            <Link to="/" className="Plan-cta">Select Plan</Link>
+                        </div>
+                        
+
+                    </div>
                 </div>
-                
 
-            </div>
-            <div className="Plan Plan3">
-                <h5 className="Plan-name">Business Premium</h5>
-                <div className="Plan-price"><span>$</span>9.99<sub>/licence/mo</sub></div>
-                <div className="Plan-FeaturesList">
-                    <div className="FeaturesList">Outlook WebAccess
-                        <span>50 GB Mailbox </span>
-                    </div>
-                    <div className="Features-icon"><figure><img src="./assets/o365_outlook.png" alt="Outlook" title="Outlook"/></figure></div>
-                    <div className="FeaturesList">Cloud Storage
-                        <span>1TB</span>
-                    </div>
-                    <div className="FeaturesList">Collaborate tools</div>
-                    <div className="Features-icon"><figure><img src="./assets/o365_teams.png" alt="team" title="Teams"/></figure><figure><img src="/assets/o365_sharepoint.png" alt="o365_sharepoint" title="sharepoint"/></figure><figure><img src="./assets/o365_yammer.png" alt="yammer" title="yammer"/></figure></div>
-                    <div className="FeaturesList">Microsoft Office online</div>
-                    <div className="Features-icon"><figure><img src="./assets/o365_word.png" alt="word" title="Word"/></figure><figure><img src="./assets/o365_excel.png" alt="excel" title="Excel"/></figure><figure><img src="./assets/o365_powerpoint.png" alt="o365_powerpoint" title="PowerPoint"/></figure></div>
-                    <div className="FeaturesList"> Microsoft Office Desktop</div>
-                    <div className="Features-icon"><figure><img src="./assets/o365_word.png" alt="word" title="Word"/></figure><figure><img src="./assets/o365_excel.png" alt="excel" title="Excel"/></figure><figure><img src="./assets/o365_powerpoint.png" alt="o365_powerpoint" title="PowerPoint"/></figure><figure><img src="./assets/o365_outlook.png" alt="Outlook" title="Outlook"/></figure><figure><img src="./assets/o365_access.png" alt="Access" title="Access"/></figure><figure><img src="./assets/o365_one_note.png" alt="note" title="Notes"/></figure><figure><img src="./assets/o365_publisher.png" alt="Publisher" title="Publisher"/></figure></div>
-                    <div className="FeaturesList">Business Apps</div>
-                    <div className="Features-icon Features-Bizicon"><figure><img src="./assets/o365_outlook_customer.png" alt="Outlook" title="Outlook"/></figure><figure><img src="./assets/o365_bookings.png" alt="o365_bookings" title="o365_bookings"/></figure><figure><img src="./assets/o365_invoicing.png" alt="o365_invoicing" title="Invoice"/></figure><figure><img src="./assets/o365_mile-iq.png" alt="o365_mile-iq" title="Mile"/></figure></div>
-                    <a href="#" className="Plan-cta">Select Plan</a>
-                </div>
-                
-
-            </div>
-        </div>
-
-    </section>
+            </section>
+        </Router>
   );
 }
 
